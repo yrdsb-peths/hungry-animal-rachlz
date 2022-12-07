1f0985a9995ab900e06ad63bbe176ed623ffb657
rchelz <83130998+rachlz@users.noreply.github.com>
2022-12-07 13:22:58 -0500

    Update #7
    Added a new background for the game and resized the elephant (still need to change the size since the elephant is the same size as the apple).

--- MyWorld.java
+++ MyWorld.java
@@ -19,6 +19,10 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1, false);
         
+        // Create a background
+        Background bg = new Background(); 
+        addObject(bg, 120, 120);
+        
         //Create the Elephant instance
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300); 
